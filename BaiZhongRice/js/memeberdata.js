d5f2e1e65aecb4a35b8c29019aab2a03dade5601
shibaiyong <shibaiyong@example.com>
2019-05-24 18:04:50 +0800

    shibaiyong change

--- BaiZhongRice/js/memeberdata.js
+++ BaiZhongRice/js/memeberdata.js
@@ -3,7 +3,13 @@ mui.init();
 //打开日期选择器
 mui('.cardstatus input').each(function(index, item) {
 	item.addEventListener('tap', function(e) {
+
+		mui.alert('验证码', '标题', '确定', function() {
+			console.log('很好')
+		}, 'div')
+
 		item.PopPicker = new mui.PopPicker();
+
 		item.PopPicker.setData([{
 			value: "first",
 			text: "全部"
@@ -19,6 +25,8 @@ mui('.cardstatus input').each(function(index, item) {
 		}])
 		item.PopPicker.show(function(SelectedItem) {
 			item.value = SelectedItem[0].text;
+			item.PopPicker.dispose();
+			item.PopPicker = null;
 		})
 	})
 })
