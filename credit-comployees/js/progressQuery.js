89a1c15b0722f3892c8fb2c8b08a9decd5570140
shibaiyong <shibaiyong@example.com>
2020-02-11 10:49:04 +0800

    shibaiyong change

--- credit-comployees/js/progressQuery.js
+++ credit-comployees/js/progressQuery.js
@@ -1,21 +1,15 @@
 
 $('.cardstatus span').click(function(){
 	$(this).addClass('bottomline').siblings().removeClass('bottomline')
-	
-		//$('.statuslist').load('checking.html .bankcardinfo')
-		
-		$.get('checking.html',function(data){
-			$(".statuslist").html(data);
-		})
 
-		
-	// 	$.ajax({
-	// 		url:'checking.html',
-	// 		type:'get',
-	// 		success:function(res){
-	// 			console.log($(res))
-	// 			console.log($('.statuslist').append($(res)))
-	// 		}
-	// 	})
-	// 
+	$.get('checking.html',function(data){
+		$(".statuslist").html(data);
+	})
+
+
+
 })
+
+
+
+mui.alert('表示已申请的信用卡正在','审批中','我知道了',null,'div')
