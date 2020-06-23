e316c26345f3b447679ad53f1d904bae23800573
shibaiyong <shibaiyong@example.com>
2020-06-23 15:58:55 +0800

    shibaiyong change

--- distributemall/js/confirmorder.js
+++ distributemall/js/confirmorder.js
@@ -1,7 +1,14 @@
 document.getElementById('submitorder').addEventListener("tap",function () {
-	mui('#popover2').popover('show')
+	mui('#popover2').popover('show');
 })
 
-mui('.mui-popover').on('tap','.mybtn',function(){
-	mui('#popover2').popover('hide')
+mui('.mui-popover').on('tap','.alipay',function(){
+	mui('#popover2').popover('hide');
+	$('.indicatecontainer').show();
 })
+
+setTimeout(function(){
+	$('.operatebtn').show()
+},5000)
+
+
